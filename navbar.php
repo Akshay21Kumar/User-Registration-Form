f5d81e15268d064ca7633830fbe4fc128dfe6476
Akshay21Kumar <144650789+Akshay21Kumar@users.noreply.github.com>
2024-08-11 18:47:14 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,20 +1,18 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary pt-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Tech Nerd</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+    <h1><a class="navbar-brand" href="index.php">Tech Coffee Break</a></h1>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="welcome.php">Home</a>
         </li>
-      </ul>
-      <form class="d-flex">
+</ul>
+    <form class="d-flex" >
         <a class="btn btn-outline-success mx-2" type="submit" href="signup.php">Signup</a>
         <a class="btn btn-outline-primary mx-2" type="submit" href="login.php">Login</a>
         <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>
-      </form>
-    </div>
+    </form>
+    
+
   </div>
 </nav>
